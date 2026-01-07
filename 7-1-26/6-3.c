fa56a3a8c0a71727d1e34ec1466c5a8d22fae794
Nirmal <nirmal@mobicip.com>
2026-01-07 18:13:17 +0530

    update 7-1-26

--- 7-1-26/6-3.c
+++ 7-1-26/6-3.c
@@ -1,3 +1,8 @@
+// Exercise 6-3. Write a cross-referencer that prints a list of all words in a document, and for
+// each word, a list of the line numbers on which it occurs. Remove noise words like ``the,''
+// ``and,'' and so on.
+
+
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
