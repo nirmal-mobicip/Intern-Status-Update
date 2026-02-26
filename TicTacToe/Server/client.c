46b5821923185e083b98040ad779c0ad558b601b
Nirmal <nirmal@mobicip.com>
2026-02-26 20:12:55 +0530

    Update 26-02-26

---
+++ TicTacToe/Server/client.c
@@ -0,0 +1,39 @@
+#include <stddef.h>
+
+typedef struct client{
+    int fd;
+    char* username;
+    int active : 1;
+    int connected : 1;
+    Match* currentMatch;
+}Client;
+
+typedef struct match{
+    int matchID;
+    int playerX;
+    int playerO;
+    char board[3][3];
+}Match;
+
+Match* createMatch(int playerX,int playerO,int mid){
+    Match* m = (Match*)malloc(sizeof(Match));
+    m->matchID = mid;
+    m->playerX = playerX;
+    m->playerO = playerO;
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            m->board[i][j] = ' ';
+        }
+    }
+    return m;
+}
+
+Client* createClient(int fd,Match* m){
+    Client* c = (Client*)malloc(sizeof(Client));
+    c->fd = fd;
+    c->username = NULL;
+    c->active = 1;
+    c->connected = 0;
+    c->currentMatch = m;
+    return c;
+}
---
+++ TicTacToe/Server/server.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<sys/epoll.h>
+
+#include "client.c"
+#include "hashmap.c"
+
+int main(){
+
+    return 0;
+}
